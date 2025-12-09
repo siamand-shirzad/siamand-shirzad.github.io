5b2efbe6f2db008e628e3635b4263570002c880a
siamand <siamand1381@gmail.com>
2025-12-09 14:19:36 +0330

    feat : implementing modalStore with zustand

---
+++ src/context/modalStore.js
@@ -0,0 +1,9 @@
+// modalStore.js
+import { create } from "zustand";
+
+export const useModalStore = create((set) => ({
+  isOpen: false,
+  openModal: () => set({ isOpen: true }),
+  closeModal: () => set({ isOpen: false }),
+  toggleModal: () => set((state) => ({ isOpen: !state.isOpen })),
+}));
