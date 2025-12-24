f9dbb94e55e9d0d13643b8cb6574f126b26b504d
siamand <siamand1381@gmail.com>
2025-12-24 19:18:46 +0330

    access to store in navbar

---
+++ src/components/navbar/MobileNavbar.jsx
@@ -0,0 +1,34 @@
+import { useModalStore } from '@/context/modalStore';
+import { Link, NavLink } from 'react-router-dom';
+
+const MobileNavbar = () => {
+  const { isOpen, toggleModal,closeModal } = useModalStore();
+  if (!isOpen) return null;
+  return (
+    <>
+      {/* <div tabIndex={0} onClick={closeModal} className='fixed inset-0 bg-black bg-opacity-50 z-30'></div> */}
+      <nav className="flex flex-col gap-4 mt-4 items-center md:hidden z-20">
+        <Link to="/#hero" className="text-white  " onClick={closeModal}>
+          Home
+        </Link>
+        <Link to="/#about" className="text-white" onClick={closeModal}>
+          About
+        </Link>
+        <Link to="/#services" className="text-white" onClick={closeModal}>
+          Services
+        </Link>
+        <Link to="/projects" className="text-white" onClick={closeModal}>
+          Projects
+        </Link>
+        <NavLink to="/blog" className="text-white" onClick={closeModal}>
+          Blog
+        </NavLink>
+        <NavLink to={'/contact'} className="btn-cta mt-2 text-glow-white" onClick={closeModal}>
+          Contact Me
+        </NavLink>
+      </nav>
+    </>
+  );
+};
+
+export default MobileNavbar;
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,12 +1,25 @@
-import { useState, useEffect, useContext } from 'react';
+import { useState, useEffect, useContext, useRef } from 'react';
 import { Link, NavLink, useNavigate } from 'react-router-dom';
 import { motion, AnimatePresence } from 'framer-motion';
 import './Navbar.css';
 import Glass from '../ui/Glass';
+import MobileNavbar from './MobileNavbar';
+import { useModalStore } from '@/context/modalStore';
+import { useShallow } from 'zustand/shallow';
 
 const Navbar = () => {
   const [isScrolled, setIsScrolled] = useState(false);
-  const [isOpen, setIsOpen] = useState(false);
+  // const { isOpen, toggleModal, closeModal } = useModalStore();
+  // const { isOpen, closeModal, toggleModal } = useModalStore(
+  //   useShallow(state => ({
+  //     isOpen: state.isOpen,
+  //     closeModal: state.closeModal,
+  //     toggleModal: state.toggleModal
+  //   }))
+  // );
+  const isOpen = useModalStore(state => state.isOpen);
+  const closeModal = useModalStore(state => state.closeModal);
+  const toggleModal = useModalStore(state => state.toggleModal);
 
   // useEffect(() => {
   //   const handleScroll = () => {
@@ -19,6 +32,7 @@ const Navbar = () => {
   // }, []);
   return (
     <>
+      <div onClick={closeModal} className="fixed inset-0 z-0  "></div>
       <header className="header fixed top-0 left-0 right-0 z-50">
         <Glass className="w-[85%] md:w-[70%]  mx-auto mt-6 px-6 border border-gray-700/50 text-white">
           <motion.div
@@ -34,16 +48,16 @@ const Navbar = () => {
               </div>
 
               <nav className="nav-center hidden md:flex  gap-8">
-                <Link to="/#hero" className="nav-link" onClick={() => setIsOpen(false)}>
+                <Link to="/#hero" className="nav-link">
                   Home
                 </Link>
-                <Link to="/#about" className="nav-link" onClick={() => setIsOpen(false)}>
+                <Link to="/#about" className="nav-link">
                   About
                 </Link>
-                <Link to="/#services" className="nav-link" onClick={() => setIsOpen(false)}>
+                <Link to="/#services" className="nav-link">
                   Services
                 </Link>
-                <Link to="/projects" className="nav-link" onClick={() => setIsOpen(false)}>
+                <Link to="/projects" className="nav-link">
                   Projects
                 </Link>
 
@@ -57,45 +71,23 @@ const Navbar = () => {
                   Contact Me
                 </NavLink>
               </div>
-              {/* hamburger menu */}
+              {/* hamburger menu icon */}
               <div className="md:hidden ">
                 <label className="burger" htmlFor="burger">
-                  <input type="checkbox" id="burger" onChange={e => setIsOpen(e.target.checked)} checked={isOpen} />
+                  <input type="checkbox" id="burger" onChange={toggleModal} checked={isOpen} />
                   <span />
                   <span />
                   <span />
                 </label>
               </div>
             </div>
-            {/* منو موبایل با انیمیشن */}
-            {isOpen && (
-              <nav className="flex flex-col gap-4 mt-4 items-center md:hidden">
-                <Link to="/#hero" className="nav-link" onClick={() => setIsOpen(false)}>
-                  Home
-                </Link>
-                <Link to="/#about" className="nav-link" onClick={() => setIsOpen(false)}>
-                  About
-                </Link>
-                <Link to="/#services" className="nav-link" onClick={() => setIsOpen(false)}>
-                  Services
-                </Link>
-                <Link to="/projects" className="nav-link" onClick={() => setIsOpen(false)}>
-                  Projects
-                </Link>
-                <NavLink to="/blog" className="nav-link" onClick={() => setIsOpen(false)}>
-                  Blog
-                </NavLink>
-                <NavLink to={'/contact'} className="btn-cta mt-2 text-glow-white" onClick={() => setIsOpen(false)}>
-                  Contact Me
-                </NavLink>
-              </nav>
-            )}
+            <MobileNavbar />
           </motion.div>
         </Glass>
       </header>
-      <div className='sticky right-0 top-110 left-0 z-40'>
+      {/* <div className='sticky right-0 top-110 left-0 z-40'>
 
-      </div>
+      </div> */}
     </>
   );
 };
